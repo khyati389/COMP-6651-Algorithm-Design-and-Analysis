94d83ed6d5e9bb648c1e381656f5ea71d723c213
khyati389 <39536386+khyati389@users.noreply.github.com>
2019-02-21 13:02:30 -0500

    Update main.cpp

--- Programming Assignment-1/main.cpp
+++ Programming Assignment-1/main.cpp
@@ -1,1 +1,107 @@
+/**
+ * Author: Khyati Chaudhary
+ * Programming Assignment 1
+ * Algorithm Design & Analysis
+ */
 
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+
+#define ROOTNODE 0
+#define MAXIMUM 2147483647
+
+int minimumPathLength = MAXIMUM;
+
+void printMatrixArray(int **matrix, int rows_cols){
+    std::cout<<"Matrix:"<<std::endl;
+    for (int i = 0; i < rows_cols; i++){
+        for (int j = 0; j < rows_cols; j++)
+            std::cout << matrix[i][j] << " ";
+        std::cout<<std::endl;
+    }
+}
+
+void printArray(int *oneDArr, int size){
+    std::cout<<"****combinations****"<<std::endl;
+    for(int i=0; i<size; i++){
+        std::cout<<oneDArr[i]<<" ";
+    }
+    std::cout<<std::endl;
+}
+
+int **initMatrix(int rows_cols){
+    int **matrix = new int*[rows_cols];
+    for (int i = 0; i < rows_cols; i++) {
+        matrix[i] = new int[rows_cols];
+        for (int j = 0; j < rows_cols; j++)
+            matrix[i][j] = 0;
+    }
+    return matrix;
+}
+
+void traverseNumbers(int *numberArray, int sizeArray, int n, int **matrix){
+    if(sizeArray == 1){
+        int root = ROOTNODE;
+        int pathweight = 0;
+        for(int i=0;i<n;i++){
+            pathweight += matrix[root][numberArray[i]];
+            root = numberArray[i];
+        }
+        pathweight = pathweight + matrix[root][ROOTNODE];
+        minimumPathLength = std::min(pathweight, minimumPathLength);
+        //printArray(numberArray,n);
+    }
+
+    for(int i=0;i<sizeArray; i++){
+        traverseNumbers(numberArray, sizeArray-1, n, matrix);
+        if(sizeArray%2 == 1){
+            std::swap(numberArray[0], numberArray[sizeArray-1]);
+        }else{
+            std::swap(numberArray[i],numberArray[sizeArray-1]);
+        }
+    }
+}
+
+int main(int argc, char* argv[]) {
+    std::ifstream fileIn(argv[1]);
+    std::ofstream fileOut(argv[2]);
+    if(!fileOut){
+        std::cout<<"wrong argument! file can't open"<<std::endl;
+        return -1;
+    }
+    if(fileIn){
+        int datasetsM;
+        fileIn>>datasetsM;
+        for(int loop = 0; loop<datasetsM; ++loop){
+            int noOfVertices,noOfEdges;
+            fileIn>>noOfVertices>>noOfEdges;
+            int *numbersLine = new int[noOfVertices - 1];
+            int k =0;
+            for(int i=0; i<noOfVertices-1; i++){
+                numbersLine[i] = ++k;
+            }
+            //printArray(numbersLine,noOfVertices-1);
+            int** matrix = initMatrix(noOfVertices);
+
+            for(int data = 0; data<noOfEdges; data++){
+                int startNode, endNode, edgeWeight;
+                fileIn>>startNode>>endNode>>edgeWeight;
+                matrix[startNode] [endNode] = edgeWeight;
+                matrix[endNode] [startNode] = edgeWeight;
+            }
+            //printMatrixArray(matrix, noOfVertices);
+            traverseNumbers(numbersLine, noOfVertices-1, noOfVertices-1, matrix);
+            std::cout<<"minimum Length:"<<minimumPathLength<<std::endl;
+            fileOut<<minimumPathLength<<std::endl;
+            delete[] matrix;
+            delete[] numbersLine;
+            minimumPathLength = MAXIMUM;
+        }
+        fileIn.close();
+    }else{
+        std::cout<<"wrong argument! file can't open"<<std::endl;
+        return -1;
+    }
+    return 0;
+}
